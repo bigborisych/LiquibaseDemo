90a4d2d4b5c449a2155517599ce8b0e6dd4742ea
Borisov Vlad <borisov.vlad27@gmail.com>
2021-10-19 15:42:44 +0300

    Initial commit

---
+++ src/main/java/ru/vladborisov/liquibasedemo/model/User.java
@@ -0,0 +1,20 @@
+package ru.vladborisov.liquibasedemo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+}
---
+++ src/test/java/ru/vladborisov/liquibasedemo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladborisov.liquibasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
